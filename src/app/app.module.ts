e6d0993174c9dc4aefe3403fd3fd2dbe10469487
Leonardo <leonardodk1@outlook.com>
2023-08-14 20:38:08 -0300

    Create: Criado component que apresenta  um unico usuario.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserListViewPageComponent } from './components/user-list-view-page/user-list-view-page.component';
+import { SingleUserViewPageComponent } from './components/single-user-view-page/single-user-view-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListViewPageComponent
+    UserListViewPageComponent,
+    SingleUserViewPageComponent
   ],
   imports: [
     BrowserModule,
