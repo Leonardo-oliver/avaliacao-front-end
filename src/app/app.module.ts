6501ed3bfbd2f0f2d8f6c6d0e59150e6be312b4f
Leonardo <leonardodk1@outlook.com>
2023-08-14 20:39:50 -0300

    Create: Criado component que cria os usuarios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserListViewPageComponent } from './components/user-list-view-page/user-list-view-page.component';
 import { SingleUserViewPageComponent } from './components/single-user-view-page/single-user-view-page.component';
+import { UserCreationPageComponent } from './components/user-creation-page/user-creation-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListViewPageComponent,
-    SingleUserViewPageComponent
+    SingleUserViewPageComponent,
+    UserCreationPageComponent
   ],
   imports: [
     BrowserModule,
